1aa37e940369aab28a1d2c3360be5d8fc204ba13
jalurkerja <phantom@jalurkerja.com>
2017-09-19 18:25:36 +0700

    aaa

--- api/func.photo_items.php
+++ api/func.photo_items.php
@@ -9,23 +9,25 @@
 		return str_replace([" ","(",")","/"],["","-","","-"],$return);
 	}
 	
-	function photo_items_list($id){
+	function photo_items_list($ids){
 		global $db;
 		$return = array();
-		$indottech_photo_items = $db->fetch_all_data("indottech_photo_items",[],"parent_id = '".$id."'");
-		$i=-1;
-		foreach($indottech_photo_items as $photo_items_list){
-			if($photo_items_list["is_childest"] == 1){
-				$i++;
-				$return[$i]["id"] = $photo_items_list["id"];
-				$return[$i]["name"] = get_complete_name($photo_items_list["id"]);
-			} else {
-				$indottech_photo_items2 = $db->fetch_all_data("indottech_photo_items",[],"parent_id = '".$photo_items_list["id"]."'");
-				foreach($indottech_photo_items2 as $photo_items_list2){
-					if($photo_items_list2["is_childest"] == 1){
-						$i++;
-						$return[$i]["id"] = $photo_items_list2["id"];
-						$return[$i]["name"] = get_complete_name($photo_items_list2["id"]);
+		foreach($ids as $id){
+			$indottech_photo_items = $db->fetch_all_data("indottech_photo_items",[],"parent_id = '".$id."'");
+			$i=-1;
+			foreach($indottech_photo_items as $photo_items_list){
+				if($photo_items_list["is_childest"] == 1){
+					$i++;
+					$return[$i]["id"] = $photo_items_list["id"];
+					$return[$i]["name"] = get_complete_name($photo_items_list["id"]);
+				} else {
+					$indottech_photo_items2 = $db->fetch_all_data("indottech_photo_items",[],"parent_id = '".$photo_items_list["id"]."'");
+					foreach($indottech_photo_items2 as $photo_items_list2){
+						if($photo_items_list2["is_childest"] == 1){
+							$i++;
+							$return[$i]["id"] = $photo_items_list2["id"];
+							$return[$i]["name"] = get_complete_name($photo_items_list2["id"]);
+						}
 					}
 				}
 			}
@@ -34,30 +36,26 @@
 	}
 	
 	function next_photo_item($group_ids,$current_id = 0){
-		foreach($group_ids as $group_id){
-			$arr = photo_items_list($group_id);
-			if($current_id > 0){
-				foreach($arr as $key => $data){
-					if($data["id"] == $current_id){
-						return $arr[$key+1]["id"];
-						break;
-					}
+		$arr = photo_items_list($group_ids);
+		if($current_id > 0){
+			foreach($arr as $key => $data){
+				if($data["id"] == $current_id){
+					return $arr[$key+1]["id"];
+					break;
 				}
-			} else {
-				return $arr[0]["id"];
 			}
+		} else {
+			return $arr[0]["id"];
 		}
 		return $arr[0]["id"];
 	}
 
 	function prev_photo_item($group_ids,$current_id = 0){
-		foreach($group_ids as $group_id){
-			$arr = photo_items_list($group_id);
-			foreach($arr as $key => $data){
-				if($data["id"] == $current_id){
-					return $arr[$key-1]["id"];
-					break;
-				}
+		$arr = photo_items_list($group_ids);
+		foreach($arr as $key => $data){
+			if($data["id"] == $current_id){
+				return $arr[$key-1]["id"];
+				break;
 			}
 		}
 		return $arr[count($arr)-1]["id"];
