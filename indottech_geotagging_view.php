b7ddc6f54de76f435eb5940b8a39fdd192e2f05f
jalurkerja <phantom@jalurkerja.com>
2017-09-21 11:24:15 +0700

    aaa

--- indottech_geotagging_view.php
+++ indottech_geotagging_view.php
@@ -1,11 +1,12 @@
 <?php include_once "head.php";?>
 	<div class="bo_title">Geotagging</div>
 	<?php
-		$user_id = $_GET["user_id"];
+		$indottech_geotagging_req_id = $_GET["indottech_geotagging_req_id"];
+		$user_id = $db->fetch_single_data("indottech_geotagging_req","user_id",["id" => $indottech_geotagging_req_id]);
 		$name = $db->fetch_single_data("users","name",["id" => $user_id]);
-		$site_id = $_GET["site_id"];
-		$sitename = $_GET["sitename"];
-		$tagging_at = $_GET["tagging_at"];
+		$site_id = $db->fetch_single_data("indottech_geotagging_req","site_id",["id" => $indottech_geotagging_req_id]);
+		$sitename = $db->fetch_single_data("indottech_geotagging_req","sitename",["id" => $indottech_geotagging_req_id]);
+		$tagging_at = $db->fetch_single_data("indottech_geotagging","tagging_at",["indottech_geotagging_req_id" => $indottech_geotagging_req_id]);
 	?>
 	Name : <b><?=$name;?></b><br>
 	Site Name : <b><?=$sitename;?></b><br>
@@ -17,9 +18,7 @@
 	<br>
 	<?php
 		$db->addtable("indottech_geotagging");
-		$db->where("user_id",$user_id);
-		$db->where("tagging_at",$tagging_at);
-		$db->where("sitename",$sitename);
+		$db->where("indottech_geotagging_req_id",$indottech_geotagging_req_id);
 		$indottech_geotaggings = $db->fetch_data(true);
 		foreach($indottech_geotaggings as $indottech_geotagging){
 			echo "<img src='geophoto/".$indottech_geotagging["filename"]."' width='200' onclick='window.open(\"geophoto/".$indottech_geotagging["filename"]."\");'>";
