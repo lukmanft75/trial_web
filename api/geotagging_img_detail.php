b93e40195939d515326ef6b84d8620d72e25658c
jalurkerja <phantom@jalurkerja.com>
2017-09-24 18:27:15 +0700

    aaa

--- api/geotagging_img_detail.php
+++ api/geotagging_img_detail.php
@@ -10,5 +10,6 @@
 	$dl_url = $_GET["dl_url"];
 	$tagging_at = $db->fetch_single_data("indottech_geotagging","tagging_at",["id" => $_GET["id"]]);
 	echo "<input type='button' value='Back' style='width:100%;height:50px;font-size:20px;font-weight:bolder;' onclick=\"window.location='geotagging_mine.php?fromlist=".$fromlist."&current=".$current."&user_id=".$user_id."&token=".$token."&site_id=".$site_id."&sitename=".$sitename."&tagging_at=".$tagging_at."&indottech_geotagging_req_id=".$indottech_geotagging_req_id."&dl_url=".$dl_url."';\">";
+	echo "<input type='button' value='Retake Photo' style='width:100%;height:50px;font-size:20px;font-weight:bolder;' onclick=\"window.location='geotagging_mine.php?fromlist=".$fromlist."&current=".$current."&user_id=".$user_id."&token=".$token."&site_id=".$site_id."&sitename=".$sitename."&tagging_at=".$tagging_at."&indottech_geotagging_req_id=".$indottech_geotagging_req_id."&dl_url=".$dl_url."&retake=".$site_id."||".$sitename."||".$tagging_at."||".$indottech_geotagging_req_id."';\">";
 	echo "<img src='../geophoto/".$db->fetch_single_data("indottech_geotagging","filename",["id" => $_GET["id"]])."'' style='width:100%;'><br>";
 ?>
