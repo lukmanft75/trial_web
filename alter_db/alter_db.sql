af715fc41ab4616e9d27f35478f8f99cc7e15eb5
Superuser <superuser@jalurkerja.com>
2017-12-13 17:47:27 +0700

    db altered

--- alter_db/alter_db.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO backoffice_menu (id,seqno,parent_id,name,url) VALUES ('108','3','92','Sites','indottech_sites_list.php');
-INSERT INTO backoffice_menu_privileges (group_id,backoffice_menu_id,privilege) VALUES ('11','108','1');
