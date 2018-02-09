be0131719a03cef3bbd067a6ed8315b57f6bd21e
jalurkerja <phantom@jalurkerja.com>
2018-02-09 16:18:41 +0700

    storage cleanup

---
+++ storage_cleanup.php
@@ -0,0 +1,9 @@
+<?php 
+	include_once "common.php";
+	$storages = $db->fetch_all_data("storage",[],"expired_at < NOW()");
+	foreach($storages as $storage){
+		$physicalname = $db->fetch_single_data("storage","physicalname",["id" => $storage["id"]]);
+		unlink("storage_share/".$physicalname);
+		$db->addtable("storage"); $db->where("id",$storage["id"]); $db->delete_();
+	}
+?>
