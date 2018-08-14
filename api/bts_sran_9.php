26e016b108f449edba66cb61057d3548ac49f752
Warih Hadi Suryono <warih_suryono@yahoo.com>
2018-08-14 21:02:35 +0700

    bts_sran_9

--- api/bts_sran_9.php
+++ api/bts_sran_9.php
@@ -1,29 +1,22 @@
 <?php 
 	include_once "header.php";
 	$atd_id = $_GET["atd_id"];
-	$bts_sran_9 = $db->fetch_all_data("indottech_bts_sran_9",[],"atd_id='".$atd_id."'")[0];
+	$bts_sran_9 = $db->fetch_all_data("indottech_bts_sran_9",[],"atd_id='".$atd_id."' ORDER BY seqno");
 	
 	if(isset($_POST["save"])){
-		// echo "<pre>";
-		// print_r($_POST);
-		// echo "</pre>";
-		
-		$db->addtable("indottech_bts_sran_9");
-		if($bts_sran_9["id"] > 0) 					$db->where("id",$bts_sran_9["id"]);
-		$db->addfield("atd_id");					$db->addvalue($atd_id);
-		$db->addfield("seqno");						$db->addvalue($_POST["seqno"]);
-		$db->addfield("description");				$db->addvalue($_POST["description"]);
-		$db->addfield("history_at");				$db->addvalue($_POST["history_at"]);
-		if($bts_sran_9["id"] > 0) $inserting = $db->update();
-		else $inserting = $db->insert();
-		
-		if($inserting["affected_rows"] > 0){
-			javascript("alert('Data berhasil disimpan');");
-			javascript("window.location=\"atp_installation_menu.php?token=".$token."&atd_id=".$atd_id."\";");
-			exit();
-		} else {
-			$_errormessage = "<font color='red'>Data gagal disimpan!</font>";
+		foreach($_POST["description"] as $seqno => $description){			
+			$db->addtable("indottech_bts_sran_9");
+			if($bts_sran_9[$seqno]["id"] > 0) 			$db->where("id",$bts_sran_9[$seqno]["id"]);
+			$db->addfield("atd_id");					$db->addvalue($atd_id);
+			$db->addfield("seqno");						$db->addvalue($seqno);
+			$db->addfield("description");				$db->addvalue($description);
+			$db->addfield("history_at");				$db->addvalue($_POST["history_at"][$seqno]);
+			if($bts_sran_9[$seqno]["id"] > 0) $inserting = $db->update();
+			else $inserting = $db->insert();
 		}
+		javascript("alert('Data berhasil disimpan');");
+		javascript("window.location=\"atp_installation_menu.php?token=".$token."&atd_id=".$atd_id."\";");
+		exit();
 	}
 ?>
 <center><b>9. DOCUMENT HISTORY</b></center>
@@ -33,52 +26,59 @@
 	<tr>
 		<td>
 			<br>
-			<table align="center" border="1">
+			<table align="center">
 				<tr>
 					<td>
-					<table border="1">
-						<tr align="center">
-							<td><b>No</b></td>
-							<td><b>Description</b></td>
-							<td><b>Date</b></td>
+						<table border="1">
+							<tr align="center">
+								<td><b>No</b></td>
+								<td><b>Description</b></td>
+								<td><b>Date</b></td>
+								</tr>
+							<tr align="left">
+								<td>1</td>
+								<td>Remove old version<br>hardware from option list</td>
+								<input type="hidden" name="description[0]" value="Remove old version hardware from option list">
+								<td><input type="date" name="history_at[0]" value="<?=$bts_sran_9[0]["history_at"];?>"></td>
+								</tr>
+							<tr align="left">
+								<td>2</td>
+								<td>Insert list of technology<br>SRAN table</td>
+								<input type="hidden" name="description[1]" value="Insert list of technology SRAN table">
+								<td><input type="date" name="history_at[1]" value="<?=$bts_sran_9[1]["history_at"];?>"></td>
+								</tr>
+							<tr align="left">
+								<td>3</td>
+								<td>Remove old installation<br>check list (BTS cabinet)</td>
+								<input type="hidden" name="description[2]" value="Remove old installation check list (BTS cabinet)">
+								<td><input type="date" name="history_at[2]" value="<?=$bts_sran_9[2]["history_at"];?>"></td>
+								</tr>
+							<tr align="left">
+								<td>4</td>
+								<td>Update check list from<br>feeder to optic cable (feeder less solution)</td>
+								<input type="hidden" name="description[3]" value="Update check list from feeder to optic cable (feeder less solution)">
+								<td><input type="date" name="history_at[3]" value="<?=$bts_sran_9[3]["history_at"];?>"></td>
+								</tr>
+							<tr align="left">
+								<td>5</td>
+								<td>Update template of <br>equipment SRAN BoQ</td>
+								<input type="hidden" name="description[4]" value="Update template of equipment SRAN BoQ">
+								<td><input type="date" name="history_at[4]" value="<?=$bts_sran_9[4]["history_at"];?>"></td>
+								</tr>
+							<tr align="left">
+								<td>6</td>
+								<td>Remove E1 transmission information</td>
+								<input type="hidden" name="description[5]" value="Remove E1 transmission information">
+								<td><input type="date" name="history_at[5]" value="<?=$bts_sran_9[5]["history_at"];?>"></td>
+								</tr>
+							<tr align="left">
+								<td>7</td>
+								<td>Additional summary number<br>of SM module, RF module and antenna</td>
+								<input type="hidden" name="description[6]" value="Additional summary number of SM module, RF module and antenna">
+								<td><input type="date" name="history_at[6]" value="<?=$bts_sran_9[6]["history_at"];?>"></td>
 							</tr>
-						<tr align="left">
-							<td>1</td>
-							<td>Remove old version<br>hardware from option list</td>
-							<td><input type="date"></td>
-							</tr>
-						<tr align="left">
-							<td>2</td>
-							<td>Insert list of technology<br>SRAN table</td>
-							<td><input type="date"></td>
-							</tr>
-						<tr align="left">
-							<td>3</td>
-							<td>Remove old installation<br>check list (BTS cabinet)</td>
-							<td><input type="date"></td>
-							</tr>
-						<tr align="left">
-							<td>4</td>
-							<td>Update check list from<br>feeder to optic cable (feeder less solution</td>
-							<td><input type="date"></td>
-							</tr>
-						<tr align="left">
-							<td>5</td>
-							<td>Update template of <br>equipment SRAN BoQ</td>
-							<td><input type="date"></td>
-							</tr>
-						<tr align="left">
-							<td>6</td>
-							<td>Remove E1 transmission information</td>
-							<td><input type="date"></td>
-							</tr>
-						<tr align="left">
-							<td>7</td>
-							<td>Additional summary number<br>of SM module, RF module and antenna</td>
-							<td><input type="date"></td>
-							</tr>
-						
-					</table>
+							
+						</table>
 					</td>
 				</tr>
 			</table>
