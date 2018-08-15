7a6e16c655d6182094c5681b85c02c14c25c15a8
lukman <lukman@corphr-nokia.com>
2018-08-15 16:51:22 +0700

    output rectifier autodownload

--- atd_rectifier_download.php
+++ atd_rectifier_download.php
@@ -1,7 +1,7 @@
 <?php
 
-	// header("Content-type: application/vnd.ms-word");
-	// header("Content-Disposition: attachment;Filename=atp_installation_rectifier.doc");
+	header("Content-type: application/vnd.ms-word");
+	header("Content-Disposition: attachment;Filename=atp_installation_rectifier.doc");
 ?>
 <html>
 	<head>
