82331e6c4ba2402639b7c9b55284b84101d7c148
lukman <lukman@corphr-nokia.com>
2018-08-21 13:30:40 +0700

    atd_bts_download open to download

--- atd_bts_download.php
+++ atd_bts_download.php
@@ -1,6 +1,6 @@
 <?php
-	// header("Content-type: application/vnd.ms-word");
-	// header("Content-Disposition: attachment;Filename=atp_bts_sran.doc");
+	header("Content-type: application/vnd.ms-word");
+	header("Content-Disposition: attachment;Filename=atp_bts_sran.doc");
 ?>
 <html>
 	<head>
@@ -8,47 +8,47 @@
 	</head>
 	<body style="margin-left:-20px; ">
 <?php
-	// include_once "atd_bts_01.php";
-	// include_once "atd_bts_02.php";
-	// include_once "atd_bts_03.php";
-	// include_once "atd_bts_04.php";
-	// include_once "atd_bts_05.php";
-	// include_once "atd_bts_06.php";
-	// include_once "atd_bts_07.php";
+	include_once "atd_bts_01.php";
+	include_once "atd_bts_02.php";
+	include_once "atd_bts_03.php";
+	include_once "atd_bts_04.php";
+	include_once "atd_bts_05.php";
+	include_once "atd_bts_06.php";
+	include_once "atd_bts_07.php";
 	include_once "atd_bts_08.php"; 
-	// include_once "atd_bts_09.php";
-	// include_once "atd_bts_10.php";
-	// include_once "atd_bts_11.php";
-	// include_once "atd_bts_12.php";
-	// include_once "atd_bts_13.php";
-	// include_once "atd_bts_14.php";
-	// include_once "atd_bts_15.php";
-	// include_once "atd_bts_16.php";
-	// include_once "atd_bts_17.php";
-	// include_once "atd_bts_18.php"; 
-	// include_once "atd_bts_19.php";
-	// include_once "atd_bts_20.php";
-	// include_once "atd_bts_21.php";
-	// include_once "atd_bts_22.php";
-	// include_once "atd_bts_23.php";
-	// include_once "atd_bts_24.php";
-	// include_once "atd_bts_25.php";
-	// include_once "atd_bts_26.php";
-	// include_once "atd_bts_27.php";
-	// include_once "atd_bts_28.php"; 
-	// // // include_once "atd_bts_29.php"; //perpanjanan halaman 28
-	// include_once "atd_bts_30.php";
-	// include_once "atd_bts_31.php";
-	// include_once "atd_bts_32.php";
-	// include_once "atd_bts_33.php";
-	// include_once "atd_bts_34.php";
-	// include_once "atd_bts_35.php";
-	// include_once "atd_bts_36.php";
-	// include_once "atd_bts_37.php";
-	// include_once "atd_bts_38.php"; 
-	// include_once "atd_bts_39.php";
-	// include_once "atd_bts_40.php";
-	// include_once "atd_bts_41.php";
+	include_once "atd_bts_09.php";
+	include_once "atd_bts_10.php";
+	include_once "atd_bts_11.php";
+	include_once "atd_bts_12.php";
+	include_once "atd_bts_13.php";
+	include_once "atd_bts_14.php";
+	include_once "atd_bts_15.php";
+	include_once "atd_bts_16.php";
+	include_once "atd_bts_17.php";
+	include_once "atd_bts_18.php"; 
+	include_once "atd_bts_19.php";
+	include_once "atd_bts_20.php";
+	include_once "atd_bts_21.php";
+	include_once "atd_bts_22.php";
+	include_once "atd_bts_23.php";
+	include_once "atd_bts_24.php";
+	include_once "atd_bts_25.php";
+	include_once "atd_bts_26.php";
+	include_once "atd_bts_27.php";
+	include_once "atd_bts_28.php"; 
+	// // include_once "atd_bts_29.php"; //perpanjanan halaman 28
+	include_once "atd_bts_30.php";
+	include_once "atd_bts_31.php";
+	include_once "atd_bts_32.php";
+	include_once "atd_bts_33.php";
+	include_once "atd_bts_34.php";
+	include_once "atd_bts_35.php";
+	include_once "atd_bts_36.php";
+	include_once "atd_bts_37.php";
+	include_once "atd_bts_38.php"; 
+	include_once "atd_bts_39.php";
+	include_once "atd_bts_40.php";
+	include_once "atd_bts_41.php";
 ?>
 </body>
 </html>
